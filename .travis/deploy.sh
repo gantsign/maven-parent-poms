5600eedbcf55b95163b0fd4e0568a878e35c9b68
John Freeman <john.freeman.opensource@gmail.com>
2019-07-29 09:56:12 +0100

    Add GPG key (#2)
    For code signing.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,8 +5,8 @@ set +x
 
 # See http://www.debonair.io/post/maven-cd/ for instructions
 openssl aes-256-cbc \
-    -K "$encrypted_83553d998fe4_key" \
-    -iv "$encrypted_83553d998fe4_iv" \
+    -K "$encrypted_06b490fdd6d5_key" \
+    -iv "$encrypted_06b490fdd6d5_iv" \
     -in .travis/codesigning.asc.enc \
     -out .travis/codesigning.asc -d
 gpg --fast-import .travis/codesigning.asc
